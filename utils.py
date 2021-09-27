277ed7fce10eeac6e796f99d0d691627ce3b06be
jmlouw <jmlouw96@gmail.com>
2021-09-27 15:09:30 +0200

    updates to RL live plots

--- utils.py
+++ utils.py
@@ -1,8 +1,8 @@
 from matplotlib import pyplot as plt
 from IPython.display import clear_output
 
-def render(env):
-    img = env.render(mode='rgb')
+def render(env, mode='rgb'):
+    img = env.render(mode=mode)
     plt.imshow(img, interpolation='nearest')
     clear_output(wait = True)
     plt.pause(0.00001)
