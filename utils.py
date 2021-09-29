171a6dbe8b27f27b9016b8b7141193e2c5ef3915
jmlouw <jmlouw96@gmail.com>
2021-09-29 13:30:26 +0200

    removed agent from heatmap plot

--- utils.py
+++ utils.py
@@ -1,29 +1,59 @@
 from matplotlib import pyplot as plt
 from IPython.display import clear_output
 import matplotlib.pyplot as plt
+from matplotlib import cm
 import numpy as np
 
+
 def render(env, mode='rgb'):
-    img = env.render(mode=mode)
+    img = env.render(mode=mode, highlight=False)
     plt.imshow(img, interpolation='nearest')
-    clear_output(wait = True)
+    clear_output(wait=True)
     plt.pause(0.00001)
-    
-def draw_triangle(p1=[1,1], p2=[2,2.5], p3= [3, 1], c='blue', alpha=0.3):
+
+
+def draw_triangle(p1=[1, 1], p2=[2, 2.5], p3=[3, 1], c='blue', alpha=0.3):
     X = np.array([p1, p2, p3])
-    Y = [c,c,c]
-    plt.scatter(X[:, 0], X[:, 1], s= 0.001, color = Y[:])
-    t1 = plt.Polygon(X[:3,:], color=Y[0], alpha=alpha)
+    Y = [c, c, c]
+    plt.scatter(X[:, 0], X[:, 1], s=0.001, color=Y[:])
+    t1 = plt.Polygon(X[:3, :], color=Y[0], alpha=alpha)
     plt.gca().add_patch(t1)
-    
-def draw_square(lb_x, lb_y, size, alpha=0.1, l_color='green', r_color='yellow', b_color='red', t_color='blue'):
+
+
+def draw_square(lb_x,
+                lb_y,
+                size,
+                alpha=1,
+                l_color='green',  # left
+                r_color='yellow',  # right
+                b_color='red',  # down
+                t_color='blue'):  # up
     tr_x = lb_x+size  # top right x
     tr_y = lb_y+size  # top right y
 
     md_x = lb_x + size/2  # middle x
     md_y = lb_y + size/2  # middle x
 
-    draw_triangle([lb_x, lb_y], [tr_x, lb_y], [md_x, md_y], c=t_color, alpha=alpha)
-    draw_triangle([lb_x, tr_y], [tr_x, tr_y], [md_x, md_y], c=b_color, alpha=alpha)
-    draw_triangle([lb_x, tr_y], [lb_x, lb_y], [md_x, md_y], c=l_color, alpha=alpha)
-    draw_triangle([tr_x, tr_y], [tr_x, lb_y], [md_x, md_y], c=r_color, alpha=alpha)
+    width = 10
+
+    md_x_plus = md_x + width/2
+    md_x_minus = md_x - width/2
+
+    md_y_plus = md_y + width/2
+    md_y_minus = md_y - width/2
+
+    colormap = cm.get_cmap('RdYlGn')
+
+    color_right = colormap(r_color)
+    color_down = colormap(b_color)
+    color_left = colormap(l_color)
+    color_up = colormap(t_color)
+
+    draw_triangle([lb_x+2, md_y], [md_x_minus, md_y_minus+2],
+                  [md_x_minus, md_y_plus-2], c=color_left, alpha=alpha)  # left
+    draw_triangle([tr_x-2, md_y], [md_x_plus, md_y_minus+2],
+                  [md_x_plus, md_y_plus-2], c=color_right, alpha=alpha)  # right
+    draw_triangle([md_x, tr_y-2], [md_x_minus+2, md_y_plus],
+                  [md_x_plus-2, md_y_plus], c=color_down, alpha=alpha)  # down
+    draw_triangle([md_x, lb_y+2], [md_x_minus+2, md_y_minus],
+                  [md_x_plus-2, md_y_minus], c=color_up, alpha=alpha)  # up
